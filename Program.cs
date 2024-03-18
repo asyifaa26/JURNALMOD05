22dcf350f1af67ae669027260256731fee43cd6d
asyifaa26 <asyifa.enk2654@gmail.com>
2024-03-18 07:16:55 +0700

    Menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+public class Penjumlahan
+{
+    public void JumlahTigaAngka<T>(T no1, T no2, T no3)
+    {
+        dynamic hasil1 = no1;
+        dynamic hasil2 = no2;
+        dynamic hasil3 = no3;
+        dynamic hasil;
+
+        hasil = hasil1 + hasil2 + hasil3;
+        Console.WriteLine("Hasil Penjumlahan: " + hasil);
+    }
+}
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Penjumlahan jumlah = new Penjumlahan();
+        jumlah.JumlahTigaAngka<long>(22, 30, 29);
+        // NIM : 1302223029
+    }
+}
--- obj/Debug/net8.0/modul5_1302223029.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul5_1302223029.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_1302223029")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e7f8f22533a9f3814ad3cc04e88985ee756330")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_1302223029")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_1302223029")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
